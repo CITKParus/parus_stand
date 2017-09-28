80a1a964ca8ad572ba3b714dbfccf5b52bc824e9
Mikhail Chechnev <mim_@mail.ru>
2017-09-28 15:08:20 +0300

    Публикация действия SHIPMENT (отгрузка со стенда) в WEB API

--- db/udo_t_web_api_actions.sql
+++ db/udo_t_web_api_actions.sql
@@ -1,5 +1,5 @@
 prompt PL/SQL Developer import file
-prompt Created on 27 Сентябрь 2017 г. by mim__000
+prompt Created on 28 Сентябрь 2017 г. by mim__000
 set feedback off
 set define off
 prompt Loading UDO_T_WEB_API_ACTIONS...
@@ -13,8 +13,10 @@ insert into UDO_T_WEB_API_ACTIONS (rn, action, processor, unauth)
 values (4, 'LOGOUT', 'UDO_PKG_WEB_API.PROCESS', 0);
 insert into UDO_T_WEB_API_ACTIONS (rn, action, processor, unauth)
 values (5, 'AUTH_BY_BARCODE', 'UDO_PKG_STAND_WEB.AUTH_BY_BARCODE', 0);
+insert into UDO_T_WEB_API_ACTIONS (rn, action, processor, unauth)
+values (6, 'SHIPMENT', 'UDO_PKG_STAND_WEB.SHIPMENT', 0);
 commit;
-prompt 5 records loaded
+prompt 6 records loaded
 set feedback on
 set define on
 prompt Done.
