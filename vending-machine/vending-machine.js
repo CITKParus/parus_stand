b162ef80002708fa3a264640408e01addb79c709
Mikhail Chechnev <mim_@mail.ru>
2017-12-12 17:35:59 +0300

    Убрал прошивку для солиноидов

--- vending-machine/vending-machine.js
+++ vending-machine/vending-machine.js
@@ -1,12 +1,22 @@
-//----------------------------------------------------
+//-------------------------------------------------------
 // Прошивка вендингового аппарата
-// Вариант для исполнительного механизма на соленоидах
-//----------------------------------------------------
+// Вариант для исполнительного механизма на сервоприводах
+//-------------------------------------------------------
 
-//-----------------------------------
-// Настраиваем константы конфигурации
-//-----------------------------------
+//------------------------------
+// Подключаем внешние библиотеки
+//------------------------------
 
+var servo = require("@amperka/servo"); //работа с сервоприводами
+var wifi = require("@amperka/wifi"); //работа с модулем WiFi
+var led = require('@amperka/led-strip'); //работа с RGB-лентой
+var http = require("http"); //работа с HTTP
+
+//--------------------------------
+// Определяем глобальные константы
+//--------------------------------
+
+//подключение к сети и параметры WEB-сервера
 var WIFI_PORT = PrimarySerial; //порт (URAT) подключения WiFi-модуля
 var SERVER_PORT = 8080; //порт WEB-сервера
 //дом
@@ -19,18 +29,7 @@ var WIFI_PSWD = "var2008eniK"; // ключ WiFi-сети
 //var WIFI_SSID = "RUDA"; // имя WiFi-сети
 //var WIFI_PSWD = "Defender0106"; // ключ WiFi-сети
 
-//------------------------------
-// Подключаем внешние библиотеки
-//------------------------------
-
-var wifi = require("@amperka/wifi"); //работа с модулем WiFi
-var http = require("http"); //работа с HTTP
-
-//--------------------------------
-// Определяем глобальные константы
-//--------------------------------
-
-//состояние ответа сервера
+//состояния ответа WEB-сервера
 var SERVER_RESP_OK = "OK"; //успех
 var SERVER_RESP_ERR = "ERR"; //ошибка
 
@@ -38,10 +37,19 @@ var SERVER_RESP_ERR = "ERR"; //ошибка
 var HARD_LINES = ["1", "2", "3"]; //наименования линий
 var LINES = [
     //конфигурация линий
-    { name: HARD_LINES[0], pin: A2 },
-    { name: HARD_LINES[1], pin: A4 },
-    { name: HARD_LINES[2], pin: P8 }
+    { name: HARD_LINES[0], pin: P8 },
+    { name: HARD_LINES[1], pin: P9 },
+    { name: HARD_LINES[2], pin: P10 }
 ];
+
+//доступные адресуемые светодиодные RGB-ленты
+var LEDSTRIP_MAIN = null; //основная лента подсветки
+var LEDSTRIP_MAIN_LENGTH = 50; //длина основной ленты подсветки (кол-во диодов)
+var LEDSTRIP_MAIN_BRIGHTNESS = 10; //яркость % от 0 - 100 основной ленты подсветки
+var LEDSTRIP_MAIN_STEP = 2; //шаг включения светодиодов (1 - каждый, 2 - каждый второй и т.п.)
+
+//конфигурация индикатора загруженности стенда
+var FULL_LOAD_PRC = 60; //% загруженности стенда, до которого он считается нормально загруженным
 
 //------------------------------
 // Функции
@@ -67,18 +75,80 @@ function buildErrResp(message) {
     return buildResp(SERVER_RESP_ERR, message);
 }
 
+//установка цвета лены
+function setColor(ledStrip, length, step, color) {
+    //выставим шаг (1 - по умолчанию)
+    var stepTmp = 1;
+    if(step) stepTmp = step;
+    //обходим указанное количество светодиодов в переданной ленте и устанавливаем нужный цвет
+    for (var i = 0; i < length; i += stepTmp) ledStrip.putColor(i, color);
+    // применяем изменения
+    ledStrip.apply();
+}
+
+//установка белого цвета ленты
+function setColorWhite(ledStrip, length, step) {
+    setColor(ledStrip, length, step, [1, 1, 1]);
+}
+
+//установка зеленого цвета ленты
+function setColorGreen(ledStrip, length, step) {
+    setColor(ledStrip, length, step, [1, 0, 0]);
+}
+
+//установка желтого цвета ленты
+function setColorYellow(ledStrip, length, step) {
+    setColor(ledStrip, length, step, [0.8, 1, 0]);
+}
+
+//установка оранжевого цвета ленты
+function setColorOrange(ledStrip, length, step) {
+    setColor(ledStrip, length, step, [0.5, 1, 0]);
+}
+
+//установка красного цвета ленты
+function setColorRed(ledStrip, length, step) {
+    setColor(ledStrip, length, step, [0, 1, 0]);
+}
+
+//установка цвета стенда в зависимости от % его загруженности
+function setColorByLoadPrc(ledStrip, length, step, loadPrc, fullPrc) {
+    //выставим шаг (1 - по умолчанию)
+    var stepTmp = 1;
+    if(step) stepTmp = step;
+    //вычислим цвета
+    var green= loadPrc/100;
+    var red = 0;
+    if (loadPrc < fullPrc) red = 1;
+    //обходим указанное количество светодиодов в переданной ленте и устанавливаем нужный цвет
+    for (var i = 0; i < length; i += stepTmp) ledStrip.putColor(i, [green, red, 0]);
+    //применяем изменения
+    ledStrip.apply();
+}
+
+//установка яркости диодов ленты
+function setBrightness(ledStrip, brightnessPrc) {
+    //выставим яркость
+    ledStrip.brightness(brightnessPrc/100);
+    //применяем изменения
+    ledStrip.apply();
+}
+
 //отгрузка с линии
 function lineShipment(lineName, callBack) {
     LINES.forEach(function(line) {
         var tmp = line;
         if (tmp.name == lineName) {
-            tmp.pin.write(true);
+            var s = servo.connect(tmp.pin);
             log("Line " + tmp.name + " opened");
+            s.write(180);
             setTimeout(function() {
-                tmp.pin.write(false);
-                log("Line " + tmp.name + " closed");
-                callBack();
-            }, 100);
+                s.write(0);
+                setTimeout(function() {
+                    log("Line " + tmp.name + " closed");
+                    callBack();
+                }, 2000);
+            }, 2000);
         }
     });
 }
@@ -93,6 +163,13 @@ function init() {
         line.pin.mode("output");
         line.pin.write(false);
     });
+    //настраиваем SPI-интерфейс для светодиодов и подключаем ленту
+    SPI2.setup({baud:3200000, mosi:B15, sck:B13, miso:B14});
+    LEDSTRIP_MAIN = led.connect(SPI2, LEDSTRIP_MAIN_LENGTH, 'RGB');
+    LEDSTRIP_MAIN.clear();
+    LEDSTRIP_MAIN.apply();
+    setBrightness(LEDSTRIP_MAIN, LEDSTRIP_MAIN_BRIGHTNESS);
+    setColorWhite(LEDSTRIP_MAIN, LEDSTRIP_MAIN_LENGTH, LEDSTRIP_MAIN_STEP);
     log("Done!");
 }
 
@@ -161,8 +238,15 @@ function processRequest(data, callBack) {
     if (data && data.query && data.query.line && HARD_LINES.indexOf(data.query.line) !== -1) {
         //открываем линию отгрузки
         log("Shiping from line " + data.query.line + "...");
+        //отгрузим товар
         lineShipment(data.query.line, function() {
             log("Shiping from line " + data.query.line + " finished");
+            //выставим подсветку
+            if (data.query.loadPrc)
+                setColorByLoadPrc(LEDSTRIP_MAIN, LEDSTRIP_MAIN_LENGTH, LEDSTRIP_MAIN_STEP, data.query.loadPrc, FULL_LOAD_PRC);
+            else
+                setColorWhite(LEDSTRIP_MAIN, LEDSTRIP_MAIN_LENGTH, LEDSTRIP_MAIN_STEP);
+            //вернем ответ
             callBack(buildOkResp("Shiped from line " + data.query.line));
         });
     } else {
